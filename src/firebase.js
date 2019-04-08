a6856bf26ed250190cdcbbe64ed82bb3f24f6f5a
TanyaRan <tatyanaran@gmail.com>
2019-04-09 02:40:06 +0300

    Initial Commit

---
+++ src/firebase.js
@@ -0,0 +1,24 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
+import 'firebase/storage';
+import {
+  apiKey,
+  authDomain,
+  databaseURL,
+  projectId,
+  storageBucket,
+  messagingSenderId
+} from './config/keys';
+
+var config = {
+  apiKey: apiKey,
+  authDomain: authDomain,
+  databaseURL: databaseURL,
+  projectId: projectId,
+  storageBucket: storageBucket,
+  messagingSenderId: messagingSenderId
+};
+firebase.initializeApp(config);
+
+export default firebase;
